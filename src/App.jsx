993819fdcb9572efb73b6a706dc3019f25bdb9b2
Manasi170806 <manasipatel17082006@gmail.com>
2025-11-26 11:16:08 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 import AppRoutes from "./routes/AppRoutes";
 import Footer from "./components/Footer";
 
